0c430b83515303e2006c762b30fd025f9633ef37
cironicizima <c.nicizima@aluno.ifsp.edu.br>
2025-02-18 17:18:33 -0300

    criacao componente logo

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+// import { Image } from 'expo-image';
 // importa algo from biblioteca react-native
+import Logo from './src/components/Logo';
+// importa o componente logo, exportado com export default.
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá Mundo React Native (EXPO)</Text>
+      <Logo/>
+      <Text style={styles.titulo}>Olá Mundo React Native (EXPO)</Text>
+      <Text style={styles.subtitulo}>Descrição do APP Cores</Text>
       <StatusBar style="auto" />
       {/* StatusBar é a barra na parte de cima do celular onde tem a hora, bateria, rede, etc */}
     </View>
@@ -15,8 +20,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo:{
+    fontSize: '18px',
+    fontWeight: 'bold'
+  },
+  subtitulo:{
+    color: 'red',
+    fontStyle: 'italic',
+    margin: 50,
+    fontWeight: 'bold',
+    fontSize: 30
   },
 });
---
+++ src/components/Logo.jsx
@@ -0,0 +1,32 @@
+import { Image } from "expo-image";
+import { View, StyleSheet, Text } from "react-native";
+
+export default function Logo() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source="https://cdn.worldvectorlogo.com/logos/react-native-1.svg"
+      />
+      <Text>Meu logo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 40
+    },
+    logo:{
+        width: 80,
+        height: 90,
+        marginBottom: 40,
+        backgroundColor: 'whitesmoke'
+    }
+})
+
+// se exportar com export default, a importaçao é direto com o nome import Funcao,
+// se exportar só com export, a importaçao é entre chaves. import { Funcao } . Entre chave pode exportar mais de uma funçao de uma vez, mas nao é a ideia pq a ideia é componentizar e ter um arquivo por funcao
